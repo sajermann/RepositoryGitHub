670f8d1c9f332977e7b6aa3ed77e9a9d6e525cc9
Bruno Sajermann <contato@sajermann.com>
2020-05-30 22:40:45 -0300

    Complete Project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Routes from './routes';
+
+import GlogalStyle from './styles/global';
+
+
+export default function App () {
+  return(
+    <>
+      <Routes />
+      <GlogalStyle />
+    </>
+  )
+}
